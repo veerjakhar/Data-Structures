3acf0c3b8949e8997216a20f382eee6ca7feb301
1eggycroissant <155930805+veerjakhar@users.noreply.github.com>
2026-06-11 20:04:29 -0700

    Add files via upload

--- Lesson-4(Linear-&-Binary-Search).py
+++ Lesson-4(Linear-&-Binary-Search).py
@@ -5,7 +5,7 @@ key = int(input("Enter the number wanted to be found: "))
 
 #Linear Search
 for i in range(0, len(data)):
-    print(data[i]) 
+    #print(data[i]) 
     if key == data[i]:
         print("^ Number found")
 
@@ -13,7 +13,7 @@ for num in data:
     if num == key:
         print("The number has been found")
 
-#Binary Search
+#Binary Search --> Method 1
 def binary(data, key, low, high):
     mid = (low + high)//2
     if low <= high:
@@ -25,3 +25,25 @@ def binary(data, key, low, high):
             return binary(data, key, low, mid - 1)
     else:
         return -1
+
+print("Found in the #{} place".format(binary(data, key, 0, len(data)) + 1))
+
+#Binary Search --> Method 2
+
+start = 0
+end = len(data) - 1
+flag = False
+
+while start <= end:
+    mid = (start + end)//2
+    if data[mid] == key:
+        print("FOUND")
+        flag = True
+        break
+    elif data[mid] > key:
+        end = mid - 1
+    elif data[mid] < key:
+        start = mid + 1
+    
+if flag == False:
+    print("The number (key) was not able to be found in the data given")
